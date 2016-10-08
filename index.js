f9dc7ad46bb96196e325b687064cfa4cc10176c2
Robin Millette <robin@millette.info>
2016-10-08 10:13:13 -0400

    Cookie auth.

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 'use strict'
 
 // npm
-require ('dotenv-safe').load({ silent: true, sample: 'node_modules/feverish-routes/.env.example' })
+require('dotenv-safe').load({
+  silent: true,
+  sample: 'node_modules/feverish-routes/.env.example'
+})
 
 const pkg = require('./package.json')
 
 const after = (options, server, next) => {
-  console.log('OPTIONS2:', options)
-
   server.views({
     engines: { html: require('lodash-vision') },
     path: 'templates',
     partialsPath: 'templates/partials',
     isCached: process.env.TEMPLATE_CACHE.toLowerCase() === 'true'
+  })
+
+  const cache = server.cache({ segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000 })
+  server.app.cache = cache
+
+  server.auth.strategy('session', 'cookie', true, {
+    password: 'password-should-be-32-characters',
+    cookie: 'sid-example',
+    redirectTo: '/testing',
+    isSecure: false,
+    validateFunc: function (request, session, callback) {
+      cache.get(session.sid, (err, cached) => {
+        if (err) {
+          return callback(err, false)
+        }
+
+        if (cached) {
+          return callback(null, true, cached.account)
+        }
+
+        return callback(null, false)
+      })
+    }
   })
 
   server.route({
@@ -22,8 +46,6 @@ const after = (options, server, next) => {
       view: {
         template: 'bienvenue',
         context: {
-          name: 'joséanne',
-          roles: ['student'],
           active: 'accueil'
         }
       }
@@ -37,8 +59,6 @@ const after = (options, server, next) => {
       view: {
         template: 'exercices',
         context: {
-          name: 'joséanne',
-          roles: ['student'],
           active: 'exercices',
           rows: []
         }
@@ -54,8 +74,6 @@ const after = (options, server, next) => {
         template: 'rendus',
         context: {
           rows: [],
-          name: 'joséanne',
-          roles: ['student'],
           active: 'rendus'
         }
       }
@@ -69,19 +87,77 @@ const after = (options, server, next) => {
       view: {
         template: 'score',
         context: {
-          name: 'joséanne',
-          roles: ['student'],
-          doc: { _id: 'aaa', _rev: '2-bbb', title: 'joséanne' },
+          doc: { _id: 'aaa', _rev: '2-bbb' },
           active: 'resultats'
         }
       }
     }
   })
 
+  let uuid = 1 // Use seq instead of proper unique identifiers for demo only
+
+  const users = {
+    john: {
+      id: 'john',
+      password: 'password',
+      name: 'John Doe',
+      roles: ['student']
+    }
+  }
+
+  const login = (request, reply) => {
+    if (request.auth.isAuthenticated) {
+      return reply.redirect('/')
+    }
+
+    let message = ''
+    let account = null
+
+    if (request.method === 'post') {
+      if (!request.payload.username || !request.payload.password) {
+        message = 'Missing username or password'
+      } else {
+        account = users[request.payload.username]
+        if (!account || account.password !== request.payload.password) {
+          message = 'Invalid username or password'
+        }
+      }
+    }
+
+    if (request.method === 'get' || message) {
+      return reply('<html><head><title>Login page</title></head><body>' +
+        (message ? '<h3>' + message + '</h3>' : '') +
+        '<form method="post" action="/testing">' +
+        'Username: <input type="text" name="username"><br>' +
+        'Password: <input type="password" name="password"><br/>' +
+        '<input type="submit" value="Login"></form></body></html>')
+    }
+
+    const sid = String(++uuid)
+    request.server.app.cache.set(sid, { account: account }, 0, (err) => {
+      if (err) {
+        reply(err)
+      }
+
+      request.cookieAuth.set({ sid: sid })
+      return reply.redirect('/')
+    })
+  }
+
+  server.route({
+    method: ['GET', 'POST'],
+    path: '/testing',
+    config: {
+      handler: login,
+      auth: { mode: 'try' },
+      plugins: { 'hapi-auth-cookie': { redirectTo: false } }
+    }
+  })
+
   server.route({
     method: 'GET',
-    path: '/testing',
-    handler: { view: { template: 'testing' } }
+    path: '/testing2',
+    handler: { view: 'testing' }
   })
 
   server.route({
@@ -94,8 +170,7 @@ const after = (options, server, next) => {
 }
 
 exports.register = (server, options, next) => {
-  console.log('OPTIONS:', options)
-  server.dependency(Object.keys(pkg.dependencies).filter((x) => pkg.notHapiDeps.indexOf(x) === -1), after.bind(null, options))
+  server.dependency(Object.keys(pkg.dependencies).filter((x) => pkg.notHapiPlugins.indexOf(x) === -1), after.bind(null, options))
   next()
 }
 
