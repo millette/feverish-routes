ca11e3477c4eacd6c3a8e2e0011a7798eb7cf0f3
Robin Millette <robin@millette.info>
2016-10-16 01:47:45 -0400

    Move functions to utils lib.

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ require('dotenv-safe').load({
   sample: 'node_modules/feverish-routes/.env.example'
 })
 
-const nano = require('nano')
-const Boom = require('boom')
-
-// core
-const url = require('url')
-
+// self
 const pkg = require('./package.json')
+const utils = require('./lib/utils')
 
-const dbUrl = url.resolve(process.env.DBURL, 'groupe2016')
-const dbUsers = url.resolve(process.env.DBURL, '_users')
-
-const isTeacher = (request) => request.auth.credentials.roles.indexOf('teacher') !== -1
+// npm
+const nano = require('nano')
 
 const after = (options, server, next) => {
   const cache = server.cache({ segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000 })
@@ -50,80 +44,23 @@ const after = (options, server, next) => {
     cache.get('accueil', (err, cached) => {
       if (err) { return reply(err) }
       if (cached) {
-        cached.editor = isTeacher(request)
+        cached.editor = utils.isTeacher(request)
         cached.active = 'accueil'
         return reply.view('bienvenue', cached).etag(cached._rev)
       }
       nano(process.env.DBURL).auth(process.env.DBUSER, process.env.DBPW, (err, body, headers) => {
         if (err) { return reply(err) }
-        nano({ url: dbUrl, cookie: headers['set-cookie'] })
+        nano({ url: utils.dbUrl, cookie: headers['set-cookie'] })
           .get('accueil', (err, body) => {
             if (err) { return reply(err) }
             cache.set('accueil', body, 0, (err) => {
               if (err) { return reply(err) }
-              body.editor = isTeacher(request)
+              body.editor = utils.isTeacher(request)
               body.active = 'accueil'
               return reply.view('bienvenue', body).etag(body._rev)
             })
           })
       })
-    })
-  }
-
-  const studentMenu = (row) => '<td><a class="label success" href="/score/' + row._id + '">Consulter mon résultat</a></td>'
-
-  const teacherMenu = (row) => [
-    '<td><a class="label" href="/edit/' + row._id + '">Éditer</a></td>',
-    '<td><a class="label success" href="/corrections/' + row._id + '">Corriger</a></td>',
-    '<td><div class="label warning" data-toggle="' + row._id + '">Effacer</div></td>',
-    '<td><div class="dropdown-pane top" id="' + row._id,
-    '" data-dropdown data-auto-focus="true" data-close-on-click="true" data-position-class="top">',
-    '<button type="button" class="confirm-delete button alert" data-exid="' + row._id + '" data-exrev="' + row._rev + '">Effacer ' + row.title + '</button>',
-    '</div></td>'
-  ].join('')
-
-  const getExercices = function (request, reply) {
-    nano(process.env.DBURL).auth(process.env.DBUSER, process.env.DBPW, (err, body, headers) => {
-      if (err) { return reply(err) }
-      nano({ url: dbUrl, cookie: headers['set-cookie'] })
-        .view('feverish', 'exercices', { 'include_docs': true, 'descending': true }, (err, body, headers) => {
-          if (err) { return reply(err) }
-          return reply({ body: body, etag: headers.etag + request.auth.credentials.name })
-        })
-    })
-  }
-
-  const exercices = function (request, reply) {
-    const body = request.pre.exercices.body
-    body.active = 'exercices'
-    body.rows = body.rows.map((r) => r.doc)
-    body.userMenu = request.auth.credentials.roles.indexOf('teacher') === -1 ? studentMenu : teacherMenu
-    return reply.view('exercices', body).etag(request.pre.exercices.etag)
-  }
-
-  const rendus = function (request, reply) {
-    const body = request.pre.exercices.body
-    body.active = 'rendus'
-    body.rows = body.rows.map((r) => r.doc)
-    return reply.view('rendus', body).etag(request.pre.exercices.etag)
-  }
-
-  const resultats = function (request, reply) {
-    const body = request.pre.exercices.body
-    body.active = 'resultats'
-    body.student = request.auth.credentials.name
-    body.self = true
-    return reply.view('etudiant', body).etag(request.pre.exercices.etag)
-  }
-
-  const rendu = function (request, reply) {
-    nano(process.env.DBURL).auth(process.env.DBUSER, process.env.DBPW, (err, body, headers) => {
-      if (err) { return reply(err) }
-      nano({ url: dbUrl, cookie: headers['set-cookie'] })
-        .attachment.get(request.params.ex, request.params.att, (e, b, h) => {
-          if (e) { return reply(e) }
-          reply(b).type(h['content-type']).etag(h.etag)
-        })
     })
   }
 
@@ -134,7 +71,7 @@ const after = (options, server, next) => {
       nano(process.env.DBURL).auth(process.env.DBUSER, process.env.DBPW, (err, body, headers) => {
         if (err) { return reply(err) }
         nano({
-          url: dbUrl,
+          url: utils.dbUrl,
           cookie: headers['set-cookie']
         }).view('feverish', type, { group: true }, (err, body) => {
           if (err) { return reply(err) }
@@ -174,57 +111,6 @@ const after = (options, server, next) => {
     })
   }
 
-  const userSorter = (a, b) => {
-    const a1 = a.name ? a.name : a.doc.name
-    const b1 = b.name ? b.name : b.doc.name
-    const a2 = a1.toLowerCase().split(' ').reverse().join(' ')
-    const b2 = b1.toLowerCase().split(' ').reverse().join(' ')
-    return a2.localeCompare(b2)
-  }
-
-  const etudiants = function (request, reply) {
-    nano(process.env.DBURL).auth(process.env.DBUSER, process.env.DBPW, (err, body, headers) => {
-      if (err) { return reply(err) }
-      nano({ url: dbUsers, cookie: headers['set-cookie'] })
-        .list({
-          include_docs: true,
-          startkey: 'org.couchdb.user:',
-          endkey: 'org.couchdb.user:\ufff0'
-        }, (err, body, headers) => {
-          if (err) { return reply(err) }
-          body.active = 'etudiants'
-          body.rows = body.rows
-            .filter((row) => row.doc.roles.indexOf('student') !== -1)
-            .sort(userSorter)
-            .map((row) => {
-              delete row.doc
-              return row
-            })
-          reply.view('etudiants', body).etag(headers.etag)
-        })
-    })
-  }
-
-  const etudiantDelete = function (request, reply) {
-    if (request.auth.credentials.roles.indexOf('teacher') === -1) {
-      return reply(Boom.forbidden('Can\'t touch this.'))
-    }
-
-    // TODO: delete rendus de référence and all other related stuff
-    nano(process.env.DBURL).auth(process.env.DBUSER, process.env.DBPW, (err, body, headers) => {
-      if (err) { return reply(err) }
-      nano({ url: dbUsers, cookie: headers['set-cookie'] })
-        .head(request.params.userid, (err, body, head) => {
-          if (err) { return reply(err) }
-          nano({ url: dbUsers, cookie: headers['set-cookie'] })
-            .destroy(request.params.userid, head.etag.slice(1, -1), (e2, b2, h2) => {
-              if (e2) { return reply(e2) }
-              reply({ ok: true })
-            })
-        })
-    })
-  }
-
   server.route({
     method: 'POST',
     path: '/logout',
@@ -244,8 +130,8 @@ const after = (options, server, next) => {
     method: 'GET',
     path: '/exercices',
     config: {
-      pre: [{ method: getExercices, assign: 'exercices' }],
-      handler: exercices
+      pre: [{ method: utils.getExercices, assign: 'exercices' }],
+      handler: utils.exercices
     }
   })
 
@@ -253,8 +139,8 @@ const after = (options, server, next) => {
     method: 'GET',
     path: '/rendus',
     config: {
-      pre: [{ method: getExercices, assign: 'exercices' }],
-      handler: rendus
+      pre: [{ method: utils.getExercices, assign: 'exercices' }],
+      handler: utils.rendus
     }
   })
 
@@ -262,27 +148,27 @@ const after = (options, server, next) => {
     method: 'GET',
     path: '/resultats',
     config: {
-      pre: [{ method: getExercices, assign: 'exercices' }],
-      handler: resultats
+      pre: [{ method: utils.getExercices, assign: 'exercices' }],
+      handler: utils.resultats
     }
   })
 
   server.route({
     method: 'GET',
     path: '/rendu/{ex}/{att}',
-    handler: rendu
+    handler: utils.rendu
   })
 
   server.route({
     method: 'DELETE',
     path: '/etudiant/{userid}',
-    handler: etudiantDelete
+    handler: utils.etudiantDelete
   })
 
   server.route({
     method: 'GET',
     path: '/etudiants',
-    handler: etudiants
+    handler: utils.etudiants
   })
 
   server.route({
