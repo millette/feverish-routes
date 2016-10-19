f802e80ee3f20d05b7fc58a8c1a3c3de48c1ca1c
Robin Millette <robin@millette.info>
2016-10-19 15:28:09 -0400

    Update welcome page.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-// npm
-const nano = require('nano')
 require('dotenv-safe').load({
   silent: true,
   sample: 'node_modules/feverish-routes/.env.example'
@@ -12,49 +10,6 @@ const pkg = require('./package.json')
 const utils = require('./lib/utils')
 
 const after = (options, server, next) => {
-  const cache = server.app.cache
-
-  const welcome = function (request, reply) {
-    cache.get('accueil', (err, cached) => {
-      if (err) { return reply(err) }
-      if (cached) {
-        cached.editor = utils.isTeacher(request)
-        return reply.view('bienvenue', cached).etag(cached.etag)
-      }
-      nano(process.env.DBURL).auth(process.env.DBUSER, process.env.DBPW, (err, body, headers) => {
-        if (err) { return reply(err) }
-        nano({ url: utils.dbUrl, cookie: headers['set-cookie'] })
-          .get('accueil', (err, body) => {
-            if (err) { return reply(err) }
-            body.active = 'accueil'
-            cache.set('accueil', body, 0, (err) => {
-              if (err) { return reply(err) }
-              body.editor = utils.isTeacher(request)
-              body.etag = body._rev + request.auth.credentials.name
-              reply.view('bienvenue', body).etag(body.etag)
-            })
-          })
-      })
-    })
-  }
-
-  const autocompleters = function (type, request, reply) {
-    cache.get(type, (err, cached) => {
-      if (err) { return reply(err) }
-      if (cached) { return reply(cached) }
-      nano(process.env.DBURL).auth(process.env.DBUSER, process.env.DBPW, (err, body, headers) => {
-        if (err) { return reply(err) }
-        nano({
-          url: utils.dbUrl,
-          cookie: headers['set-cookie']
-        }).view('feverish', type, { group: true }, (err, body) => {
-          if (err) { return reply(err) }
-          cache.set(type, body.rows, 0, (err) => reply(err || body.rows))
-        })
-      })
-    })
-  }
-
   server.views({
     engines: { html: require('lodash-vision') },
     path: 'templates',
@@ -65,7 +20,17 @@ const after = (options, server, next) => {
   server.route({
     method: 'GET',
     path: '/',
-    handler: welcome
+    handler: utils.welcome
+  })
+
+  server.route({
+    method: 'POST',
+    path: '/',
+    config: {
+      plugins: { hapiAuthorization: { roles: ['teacher'] } },
+      payload: { parse: false },
+      handler: utils.welcomePost
+    }
   })
 
   server.route({
@@ -133,13 +98,13 @@ const after = (options, server, next) => {
   server.route({
     method: 'GET',
     path: '/theme.json',
-    handler: autocompleters.bind(null, 'themes')
+    handler: utils.autocompleters.bind(null, 'themes')
   })
 
   server.route({
     method: 'GET',
     path: '/travail.json',
-    handler: autocompleters.bind(null, 'travaux')
+    handler: utils.autocompleters.bind(null, 'travaux')
   })
 
   server.route({
